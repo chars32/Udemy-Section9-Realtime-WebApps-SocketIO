9ccf08247be69abf84e44ab5c155a2a770cfe6b4
Carlos Garcia <chars24@gmail.com>
2017-11-12 23:47:00 -0600

    9.11 Printing Messages Timestamps

--- playground/time.js
+++ playground/time.js
@@ -6,5 +6,10 @@ var moment = require('moment');
 // var date = moment();
 // date.add(1, 'years').subtract(1, 'months');
 // console.log(date.format('MMM Do YYYY'))
-var date = moment();
+
+var someTimeStamp = moment().valueOf();
+console.log(someTimeStamp);
+
+var createdAt = 1234;
+var date = moment(createdAt);
 console.log(date.format('h:mm a'))
