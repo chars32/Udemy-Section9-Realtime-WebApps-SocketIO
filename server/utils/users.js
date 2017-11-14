8c23f88db13276b2291c434c3b3f775354da768d
Carlos Garcia <chars24@gmail.com>
2017-11-14 00:06:02 -0600

    9.16 Storing Users With ES6 Classes (1 & 2)

---
+++ server/utils/users.js
@@ -0,0 +1,35 @@
+// Aqui crearemos la clase Users con ES6, la cual contendra
+// un objeto con los usuarios que entren al room, aqui tambien
+// definiremos los metodos para agregar, eliminar, obtener 1 y 
+// obtener una lista de usuarios.
+
+class Users {
+  constructor () {
+    this.users = [];
+  }
+  addUser (id, name, room) {
+    var user = {id, name, room};
+    this.users.push(user);
+    return user;
+  }
+  removeUser (id) {
+    var user = this.getUser(id);
+
+    if (user) {
+      this.users = this.users.filter((user) => user.id !== id);
+    }
+
+    return user;
+  }
+  getUser (id) {
+    return this.users.filter((user) => user.id === id)[0]
+  }
+  getUserList (room) {
+    var users = this.users.filter((user) => user.room === room);
+    var namesArray = users.map((user) => user.name);
+
+    return namesArray;
+  }
+}
+
+module.exports = {Users};
