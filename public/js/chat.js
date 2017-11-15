bf2d4ebd798a51646dce4c3c37dc2d2740a9ec19
Carlos Garcia <chars24@gmail.com>
2017-11-14 23:18:38 -0600

    9.17 Wiring Up User List

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,11 +1,8 @@
 var socket = io();
 
 function scrollToBottom () {
-  // Selectors
   var messages = jQuery('#messages');
   var newMessage = messages.children('li:last-child');
-  // Heights
-  // prop viene de jquery
   var clientHeight = messages.prop('clientHeight');
   var scrollTop = messages.prop('scrollTop');
   var scrollHeight = messages.prop('scrollHeight');
@@ -17,10 +14,9 @@ function scrollToBottom () {
   }
 }
 
+// ---- from server to client ----
 socket.on('connect', function () {
   var params = jQuery.deparam(window.location.search);
-  // emitimos un evento con nombre join el cual tiene 
-  // params y un callback como parametros.
   socket.emit('join', params, function (err) {
     if (err) {
       alert(err);
@@ -31,10 +27,19 @@ socket.on('connect', function () {
   });
 });
 
-// ---- from server to client ----
 socket.on('disconnect', function () {
   console.log('Disconnected from server')
 });
+// escuchamos el evento updateUserList y lo mandamos al html
+socket.on('updateUserList', function (users) {
+  var ol = jQuery('<ol></ol>');
+
+  users.forEach(function (user) {
+    ol.append(jQuery('<li></li>').text(user));
+  });
+
+  jQuery('#users').html(ol);
+})
 
 socket.on('newMessage', function (message){
   var formattedTime = moment(message.createdAt).format('h:mm a')
--- server/server.js
+++ server/server.js
@@ -4,13 +4,17 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const {generateMessage, generateLocationMessage} = require('./utils/message.js');
-// llamamos al validador isRealString del archivo validation.js
 const {isRealString} = require('./utils/validation');
+// Llamos a Users del archivo utils/users.js
+const {Users} = require('./utils/users');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 const app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
+// instanciamos Users en la variable users
+// para asi poder usar sus metodos.
+var users = new Users();
 
 // Middleware
 app.use(express.static(publicPath));
@@ -21,29 +25,33 @@ io.on('connection', (socket) => {
   
   // ---- from client to server ----.
   socket.on('join', (params, callback) => {
-
     if (!isRealString(params.name) || !isRealString(params.room)) {
-      callback('Name and room name are required.');
+      return callback('Name and room name are required.');
     }
-
+    // se une al room
     socket.join(params.room);
+    // eliminamos al user si esta en otro room.
+    users.removeUser(socket.id);
+    // agregamos al user al room.
+    users.addUser(socket.id, params.name, params.room);
+    // enviamos al room la lista actualizada.
+    io.to(params.room).emit('updateUserList', users.getUserList(params.room));
 
-    //io.emit <--- a todos los que esten conectados
-    //socket.broadcast.emit <--- lo manda a todos conectados al socket excepto al usuario que lo manda.
-    //socket.emit <--- manda el mensaje especificamente a un usuario.
-
-    // --- Para los rooms
-    //io.to('name room').emit <--- a todos los que esten conectados a ese room
-    //socket.broadcast.to('name room').emit <--- lo manda a todos conectados al socket del room excepto al usuario que lo manda.
-
-    // Movimos estos emit para que solo sean visto por las personas que estan en el room
     socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
-    socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} hass joined`));
+    socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined`));
     
     callback();
   });
   socket.on('disconnect', () => {
-    console.log('User was disconnected')
+    // eliminamos al usuario de la lista
+    var user = users.removeUser(socket.id);
+
+    if (user) {
+      // actualizamos la lista y la mandamos al room
+      io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+      // emitimos un nuevo mensaje de que ha salido dle room
+      io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left`));
+    }
   });
   socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);     
@@ -53,9 +61,6 @@ io.on('connection', (socket) => {
   socket.on('createLocationMessage', (coords) => {
     io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude))
   });
-  // ---- from server to client ----
-  // socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
-  // socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 })
 
 server.listen(port, () => {
